0a392980b6b123a50ea403970ff71fefff2efbce
bizonlee <bizonlee@gmail.com>
2018-12-01 15:46:30 +0300

    урок 2 две точки процедурой

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,7 +6,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Console.WriteLine("Hello World!;");
+            int x = 1;
+            int y = 3;
+            char sym1 = '*';
+
+            Console.SetCursorPosition(x,y);
+            Console.Write(sym1);
+
+            int x2 = 9;
+            int y2 = 4;
+            char sym2 = '#';
+
+            Console.SetCursorPosition(x2, y2);
+            Console.Write(sym2);
+
+
+
             Console.ReadKey();
         }
     }
