19411581d571e38fa67df8612a57fe4980adadd1
bizonlee <bizonlee@gmail.com>
2018-12-01 19:54:31 +0300

    создал лист. ознакомился с циклом foreach

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Snake
 {
@@ -7,10 +8,22 @@ namespace Snake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point(1,2, '*');
-            p1.Draw();
+           Point p1 = new Point(1,2, '*');
+          //  p1.Draw();
             Point p2 = new Point(9,15, '#');
-            p2.Draw();
+          //  p2.Draw();
+            
+
+            List<Point> pointList = new List<Point>();
+            pointList.Add(p1);
+            pointList.Add(p2);
+
+            pointList[1].Draw();
+            foreach (Point i in pointList)
+            {
+                i.Draw();
+              
+            }
             Console.ReadKey();
         }
        
