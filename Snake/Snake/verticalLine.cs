2f835e7bdd3f89cba495410d78cc94f00b7c1b18
bizonlee <bizonlee@gmail.com>
2018-12-02 15:10:21 +0300

    создал вертикальную линию по уроку

---
+++ Snake/Snake/verticalLine.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snake
+{
+    
+
+    class verticalLine
+    {
+        List<Point> pointList;
+
+        public verticalLine(int x, int yTop, int yBottom,  char sym)
+        {
+            pointList = new List<Point>();
+            for (int y = yTop; y <= yBottom; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pointList.Add(p);
+            }
+            
+        }
+
+        public void Draw()
+        {
+            foreach (Point p in pointList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
